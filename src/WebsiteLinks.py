a881c37e33367e1714d57441099f24173aadfecd
Rubens Peculis <rubens@rubenspeculis.com>
2012-08-15 16:03:37 +1000

    Now makes sure that the link is on the same domain prior to continuing

--- src/WebsiteLinks.py
+++ src/WebsiteLinks.py
@@ -2,14 +2,10 @@
 
 # get_links.py
 
-import re
 import sys
 import urllib
 from urlparse import urlparse, urljoin
 from BeautifulSoup import BeautifulSoup
-
-class MyOpener(urllib.FancyURLopener):
-    version = 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15'
 
 def process(url):
     page = urllib.urlopen(url)
@@ -18,24 +14,27 @@ def process(url):
     page.close()
 
     soup = BeautifulSoup(text)
-
+    netloc = urlparse(url)
+    
     results = []
 
     for tag in soup.findAll('a', href=True):
         temp = urlparse(tag['href'])
+
         if temp.scheme != 'http':
             tag['href'] = urljoin(url, tag['href'])
-            results.append(tag['href'])
+            current = urlparse(tag['href'])
+            if current.netloc == netloc.netloc:
+                results.append(tag['href'])
 
     return results
 # process(url)
 
 def main():
     if len(sys.argv) == 1:
-        print "Jabba's Link Extractor v0.1"
+        print "Rubens' Link Extractor v0.2"
         print "Usage: %s URL [URL]..." % sys.argv[0]
         sys.exit(-1)
-
 
     # else, if at least one parameter was passed
     
@@ -45,15 +44,13 @@ def main():
     for i in range(len(a)):
         print a[i]
         path = a[i]
-        if a[i] != 'mailto:contactus@myregion.gov.au':
-            b = process(path)
-            for k in range(len(b)):
-                print '----' + b[k]
-                path2 = b[k]
-                if b[k] != 'mailto:contactus@myregion.gov.au':
-                    c = process(path2)
-                    for m in range(len(c)):
-                        print '--------' + c[m]
+        b = process(path)
+        for k in range(len(b)):
+            print '----' + b[k]
+            path2 = b[k]
+            c = process(path2)
+            for m in range(len(c)):
+               print '--------' + c[m]
 
 # main()
 
